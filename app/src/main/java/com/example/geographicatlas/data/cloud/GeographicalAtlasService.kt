79ceb36f5b4bc03ff4e35f0d0314a82ea5218801
sardoriiiy <leo@indev.expert>
2024-09-30 10:12:26 +0500

    Revert "my changes"
    This reverts commit 5eca71b6ba46144cfbfcda614866640eac9759e1.

--- app/src/main/java/com/example/geographicatlas/data/cloud/GeographicalAtlasService.kt
+++ app/src/main/java/com/example/geographicatlas/data/cloud/GeographicalAtlasService.kt
@@ -3,7 +3,6 @@ package com.example.geographicatlas.data.cloud
 import retrofit2.Response
 import retrofit2.http.*
 
-
 interface GeographicalAtlasService {
 
     @GET("v3.1/all")
@@ -13,42 +12,4 @@ interface GeographicalAtlasService {
     suspend fun fetchCountryDetails(
         @Path("cca2") cca2: String,
     ): Response<List<Any>>
-
-
-}
-
-const val contentType = "Content-Type:  application/octet-stream"
-interface TestService {
-
-    @Headers(contentType)//v7
-    @POST("get_info")
-    suspend fun getInfo(@Body body: String): Response<String>
-
-    @Headers(contentType)//v8.3
-    @POST("x_report")
-    suspend fun xReport(@Body body: String): Response<String>
-
-    @Headers(contentType)//v8.3
-    @POST("sale")
-    suspend fun sale(@Body body: String): Response<String>
-
-    @Headers(contentType)//v8.3
-    @POST("check_shift")
-    suspend fun checkShift(@Body body: String): Response<String>
-
-    @Headers(contentType)//v8.3
-    @POST("close_shift")
-    suspend fun closeShift(@Body body: String): Response<String>
-
-    @Headers(contentType)//v8.3
-    @POST("open_shift")
-    suspend fun openShift(@Body body: String): Response<String>
-
-    @Headers(contentType)//v8.3
-    @POST("deposit")
-    suspend fun deposit(@Body body: String): Response<String>
-
-    @Headers(contentType)//v8.3
-    @POST("withdraw")
-    suspend fun withdraw(@Body body: String): Response<String>
 }
--- app/src/main/java/com/example/geographicatlas/data/cloud/RetrofitInstance.kt
+++ app/src/main/java/com/example/geographicatlas/data/cloud/RetrofitInstance.kt
@@ -5,9 +5,6 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.converter.scalars.ScalarsConverterFactory
-import java.time.Duration
-import java.util.concurrent.TimeUnit
 
 class RetrofitInstance {
 
@@ -23,30 +20,18 @@ class RetrofitInstance {
 
     private val client: OkHttpClient = OkHttpClient.Builder()
         .apply {
-            addInterceptor(interceptor)
-                connectTimeout(1, TimeUnit.MINUTES)
-                readTimeout(1, TimeUnit.MINUTES)
-                writeTimeout(1, TimeUnit.MINUTES)
+            if (BuildConfig.DEBUG) {
+                addInterceptor(interceptor)
+            }
         }
         .build()
 
     fun service(): GeographicalAtlasService {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .addConverterFactory(ScalarsConverterFactory.create())
             .addConverterFactory(GsonConverterFactory.create())
             .client(client)
             .build()
             .create(GeographicalAtlasService::class.java)
-    }
-
-    fun serviceTest(): TestService {
-        return Retrofit.Builder()
-            .baseUrl("http://192.168.3.177:8008/")
-            .addConverterFactory(ScalarsConverterFactory.create())
-            .addConverterFactory(GsonConverterFactory.create())
-            .client(client)
-            .build()
-            .create(TestService::class.java)
     }
 }
